2279ebccd7cd4499e0121ef1e1f77de39e291726
Rich Jones <jones_rich@yahoo.com>
2017-05-20 12:08:57 -0500

    basics of email communication are up and running

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Mail\Welcome;
+use Illuminate\Http\Request;
+use Illuminate\Mail\Mailer;
+
+/**
+ * Class EmailController.
+ */
+class EmailController extends Controller
+{
+    /**
+     * @var Welcome
+     */
+    private $welcomeEmail;
+    /**
+     * @var Mailer
+     */
+    private $Mailer;
+
+    /**
+     * EmailController constructor.
+     *
+     * @param Welcome $welcomeEmail
+     * @param Mailer  $Mailer
+     */
+    public function __construct(
+        Welcome $welcomeEmail,
+        Mailer $Mailer)
+    {
+        $this->setWelcomeEmail($welcomeEmail);
+        $this->setMailer($Mailer);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function welcomeEmail(Request $request)
+    {
+        // need to create the Welcome email first
+        $welcomeEmail = $this->getWelcomeEmail();
+
+        // send/queue the email.
+        $this->getMailer()
+            ->to($request->get('email'))
+            ->queue($welcomeEmail);
+
+        return redirect()->back();
+    }
+
+    /**
+     * @return Welcome
+     */
+    public function getWelcomeEmail(): Welcome
+    {
+        return $this->welcomeEmail;
+    }
+
+    /**
+     * @param Welcome $welcomeEmail
+     *
+     * @return EmailController
+     */
+    public function setWelcomeEmail(Welcome $welcomeEmail): EmailController
+    {
+        $this->welcomeEmail = $welcomeEmail;
+
+        return $this;
+    }
+
+    /**
+     * @return Mailer
+     */
+    public function getMailer(): Mailer
+    {
+        return $this->Mailer;
+    }
+
+    /**
+     * @param Mailer $Mail
+     *
+     * @return EmailController
+     */
+    public function setMailer(Mailer $Mail): EmailController
+    {
+        $this->Mailer = $Mail;
+
+        return $this;
+    }
+
+    /**
+     * @return Welcome
+     */
+    protected function getNewWelcomeEmail(): Welcome
+    {
+        $welcomeEmail = $this->getWelcomeEmail();
+        $welcomeEmail = new $welcomeEmail();
+
+        return $welcomeEmail;
+    }
+}
---
+++ resources/views/email/welcome.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to Swing Low Option Trading
+
+Thanks for signing up... You will receive an email alert notifying you of both open and closed trades they happen...
+
+
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
