29b5ccfa84a50dc9d256df5aed6347c8ecabdcbd
Rich Jones <jones_rich@yahoo.com>
2017-05-20 18:55:27 -0500

    adding email on the front end complete.  Need sweet alert notification next

--- resources/assets/js/ezSlot.js
+++ resources/assets/js/ezSlot.js
@@ -3,9 +3,13 @@
 require('./bootstrap');
 
 // ezSlot
+
+// - keep vue-closed-trades in this file as first component.  I keeps the type face in the table for
+//   vue-closed-trades2.vue to a mono type font... TODO: fix this...
 Vue.component('vue-closed-trades', require('./components/app/closedTrades/vue-closed-trades.vue'));
 Vue.component('vue-closed-trades2', require('./components/app/closedTrades/vue-closed-trades2.vue'));
+Vue.component('welcome-leads-email', require('./components/app/welcomeLeads/welcome-leads-email.vue'));
 
 new Vue({
-    el: '#portfolio'
+    el: '#vueAppScope'
 });
